0d6e0dff8ee6d4db8e530df89e075a9842bc3797
seungjun0423 <sibonum@gmail.com>
2022-10-12 17:40:06 +0900

    add css

---
+++ react-leaflet/src/data/stationOptionData.js
@@ -0,0 +1,7 @@
+export const stationTypes = [
+  { id: 1, typeName: "전체" },
+  { id: 2, typeName: "aws" },
+  { id: 3, typeName: "asos" },
+  { id: 4, typeName: "rda" },
+  { id: 5, typeName: "fireblight" },
+];
